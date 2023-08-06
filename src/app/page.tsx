4a529c0eaddbb69d909911c8b9f66f8592966fe0
Satria Agung Nugraha <satrianugrahacode@gmail.com>
2023-08-07 00:53:10 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,7 @@ import { users } from "@/db/schema";
 
 export default async function Home() {
   const result = await db.select().from(users);
-  return <div>{JSON.stringify(result)}</div>;
+  return <div>
+    {JSON.stringify(result)}
+  </div>;
 }
