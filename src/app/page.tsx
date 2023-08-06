dc44e2b138c70a7fe95f14b35850ba800f30ab45
Satria Agung Nugraha <satrianugrahacode@gmail.com>
2023-08-07 01:48:29 +0700

    add tanstack query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { db } from "@/db";
 import { users } from "@/db/schema";
+import Link from 'next/link';
 
-export default async function Home() {
-  const result = await db.select().from(users);
-  return <div>
-    {JSON.stringify(result)}
-  </div>;
+// export default async function Home() {
+//   const result = await db.select().from(users);
+//   return <div>
+//     {JSON.stringify(result)}
+//   </div>;
+// }
+
+export default function Home() {
+  return <Link href='posts'>Posts</Link>;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 import { users } from "@/db/schema";
 import { InferModel } from "drizzle-orm";
+export * from './apiTypes'
 export type User = InferModel<typeof users, "select">;
+
