45e86d34f3ae62b402e6f134ff9d5df3c30ee175
Satria Agung Nugraha <satrianugrahacode@gmail.com>
2023-08-09 21:58:16 +0700

    first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "_codesan",
+    url: "https://ui.shadcn.com",
+    ogImage: "https://ui.shadcn.com/og.jpg",
+    description:
+      "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    links: {
+      twitter: "https://twitter.com/_codesan",
+      github: "https://github.com/codesan-git",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
